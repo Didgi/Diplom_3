80ce5a23625e7d409214a62f033197f5724ac5ee
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2024-02-03 13:33:55 +0300

    add api implementation for preparing,cleaning data

---
+++ src/main/java/ru/praktikum/yandex/api/configs/Configs.java
@@ -0,0 +1,9 @@
+package ru.praktikum.yandex.api.configs;
+
+public class Configs {
+    public static final String CREATE_USER = "/api/auth/register";
+    public static final String LOGIN_USER = "/api/auth/login";
+    public static final String USER = "/api/auth/user";
+    public static final String DELETE_CONFIRM_MESSAGE = "User successfully removed";
+}
+
